361de2f343899da3f4df97f9b76f876af1eb219d
Kyle Grondin <kagron12@gmail.com>
2017-07-25 16:32:05 +0000

    Added mailer view, form validation, and finished styles

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,28 @@ class ContactsController < ApplicationController
   end
   
   def create
-    
+   # Mass assignment of form fields into a contact object
+      @contact = Contact.new(contact_params)
+      if @contact.save
+         # Save the object to a database and if it goes through, send email
+         name = params[:contact][:name]
+         email = params[:contact][:email]
+         body = params[:contact][:inquiry]
+         # plug variables into Contact mailer email method
+         ContactMailer.contact_email(name, email, body).deliver
+         #Store success message in flash hash
+         flash[:success] = "Message sent"
+         redirect_to new_contact_path
+      else 
+         # If save fails, show errors in readable sentences
+         flash[:danger] = @contact.errors.full_messages.join(", ")
+         redirect_to new_contact_path
+      end
   end
+   
+   private
+   # To collect data from form, we need to use strong parameters
+      def contact_params
+         params.require(:contact).permit(:name, :email, :inquiry)
+      end
 end
