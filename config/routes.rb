3a1d26528b1fa4c1dd31374a98a70d3d381229bc
Kyle Grondin <kagron12@gmail.com>
2017-07-25 16:03:57 +0000

    Added contact controller, routes, model, and table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#index'
+  resources :contacts, only: :create
+  get 'contact', to: 'contacts#new', as: 'new_contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
