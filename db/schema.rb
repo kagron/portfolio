0d8ee3259bcdb153abb824a009f4e1edfd3b7837
Kyle Grondin <kagron12@gmail.com>
2018-01-12 17:22:01 +0000

    Added courses to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180103165631) do
+ActiveRecord::Schema.define(version: 20180109150007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 20180103165631) do
     t.text     "inquiry"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
